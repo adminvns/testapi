1a579c009e437ad8be2a3525cb5439a34a3a9114
admivns <shubham.editor@gmail.com>
2021-04-16 03:13:56 +0530

    update

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,4 +1,4 @@
-var express = require('express');
+const express = require('express');
 const router = express.Router();
 
 router.get('/',(req, res, next) =>{
@@ -12,12 +12,48 @@ res.status(200).json({
 
 router.post('/',(req, res, next) =>{
 
-    res.status(200).json({
+    res.status(201).json({
     
         message: "Products(post method)"
     });
-    
     } );
 
+
+
+ router.get('/:productID',(req, res, next)=> {
+
+    const id = req.params.productID;
+    if(id==123){
+
+        res.status(200).json({
+            message: "congratulations on winning it",
+            id: id
+        });
+    
+    }else{
+
+        res.status(200).json({
+
+            message: "this is random id" 
+        });
+    }
+
+ });   
+
+
+ router.patch('/:productID',(req, res, next)=> {
+        res.status(200).json({
+
+            message :" Product updated successfully."
+        })
+ }); 
+
+ 
+ router.delete('/:productID',(req, res, next)=> {
+    res.status(200).json({
+
+        message :" Product deleted successfully."
+    })
+}); 
 
 module.exports = router;
--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 
 const prodRoute = require('./api/routes/products')
+const prodOrder = require('./api/routes/orders')
 
-
-app.use('/products', prodRoute)
+app.use('/products', prodRoute);
+app.use('/orders',prodOrder);
 app.use('/', (req, res , next)=>{
 
 res.status(200).json({
@@ -12,12 +13,6 @@ res.status(200).json({
 });
 
 });
-// app.use((req, res, next)=> {
-//     res.status(200).json({
-//         message: "It works!"
-//     });
-    
 
-// });
 
 module.exports= app;
