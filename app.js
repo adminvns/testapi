3b9e838130df1c4caaefa7e25b022826e19bb2c3
adminvns <shubham.editor@gmail.com>
2021-04-16 19:02:25 +0530

    adding error management

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 var express = require('express');
 var app = express();
-
+const morgan = require('morgan')
 const prodRoute = require('./api/routes/products')
 const prodOrder = require('./api/routes/orders')
+//rerouting requests
 
+app.use(morgan('dev'));
 app.use('/products', prodRoute);
 app.use('/orders',prodOrder);
-app.use('/', (req, res , next)=>{
+// app.use('/', (req, res , next)=>{
 
-res.status(200).json({
-    message: "default page!"
+// res.status(200).json({
+//     message: "default page!"
+// });
+
+// });
+
+
+//error handling
+app.use((req,res,next)=>{
+
+    const err = new Error('Page Not Found');
+    err.status = 404;    
+    next(err);
+
+
+})
+app.use((err,req,res,next)=>{
+
+    res.status(err.status || 500)
+    res.json({
+            error:{
+                message: err.message
+            },
+    });
 });
 
-});
-//test
 
 module.exports= app;
