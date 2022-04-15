b5e66acbb51635b5c68011fe7f420d7dd628ec27
ShubhamAdmin <38102134+adminvns@users.noreply.github.com>
2022-04-15 22:33:15 +0530

    update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app  = require('./app');
 const http = require('http');
-const port = process.env.PORT || 3000;
+const port = 5000;
 const server = http.createServer(app);
+console.log('Server is running on port ' + port);
 server.listen(port);
