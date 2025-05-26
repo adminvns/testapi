6b0a8b636f2db78d8fa3b48db1a2e05c601df399
ShubhamAdmin <38102134+adminvns@users.noreply.github.com>
2025-05-26 23:55:27 +0530

    add uptime in healthcheck api

--- api/routes/healthCheck.js
+++ api/routes/healthCheck.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+function formatUptime(seconds) {
+  const mins = Math.floor(seconds / 60);
+  const secs = Math.floor(seconds % 60);
+  return `Up from past ${mins} minute${mins !== 1 ? 's' : ''} ${secs} second${secs !== 1 ? 's' : ''}`;
+}
+
 router.get('/', (req, res) => {
+  const uptimeInSeconds = process.uptime();
+
   res.status(200).json({
     status: 'ok',
     message: 'Health check passed Successfully!',
+    uptime: formatUptime(uptimeInSeconds),
     timestamp: new Date().toISOString(),
   });
 });
