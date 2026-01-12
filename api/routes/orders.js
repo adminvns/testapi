2fceb4d055871f8ba27716a6035b47f25a8edf77
adminvns <shubham.editor@gmail.com>
2026-01-13 00:27:18 +0530

    feat: add linter flow (#61)

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,42 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req, res, next)=> {
+router.get('/', (req, res) => {
 
-res.status(200).json({
+  res.status(200).json({
 
-    message: "Order was fetched"
+    message: 'Order was fetched'
+  });
+
 });
 
+router.post('/', (req, res) => {
+
+  res.status(201).json({
+
+    message: 'Order was created'
+  });
+
 });
 
-router.post('/',(req, res, next)=> {
 
-    res.status(201).json({
-    
-        message: "Order was created"
-    });
-    
-    });
+router.get('/:orderID', (req, res) => {
 
+  res.status(200).json({
+    message: 'order details',
+    id: req.params.orderID
+  });
 
-router.get('/:orderID',(req, res, next)=>{
+});
 
-    res.status(200).json({
-        message: "order details",
-        id: req.params.orderID
-    })
+router.delete('/:orderID', (req, res) => {
 
-})
+  res.status(200).json({
+    message: 'order deleted',
+    id: req.params.orderID
+  });
 
-router.delete('/:orderID',(req, res, next)=>{
-
-    res.status(200).json({
-        message: "order deleted",
-        id: req.params.orderID
-    })
-
-})
+});
 
 
 module.exports = router;
